407d6ac23bffbfee7ecf62edb0e02ebcb8090f13
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-04-01 22:43:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 require 'pry'
 
-def reverse_each_word(sentence1)
-  sentence1.each { |string|
-  string.reverse
-  }
- 
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  binding.pry 
 end 
