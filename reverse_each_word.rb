526c4c0154f16fd284546e6c9571130ef9e756da
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-04-01 22:47:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,12 @@
 require 'pry'
 
 def reverse_each_word(sentence)
+  empty_array = [] 
   sentence_array = sentence.split(" ")
   sentence_array.each { |string| 
-  string.reverse 
+  empty_array << string.reverse 
   binding.pry 
   }
+  empty_array.join(" ")
 
 end 
