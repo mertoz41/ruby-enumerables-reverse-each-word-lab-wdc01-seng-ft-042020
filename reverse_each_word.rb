f6e0cd4d9d7753faab51833eb9535699718e7404
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-04-01 22:52:53 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+def reverse_each_word(sentence)
+  empty_array = [] 
+  sentence_array = sentence.split(" ")
+  sentence_array.each { |string| 
+  empty_array << string.reverse 
+  
+  }
+  empty_array.join(" ")
+
+end 
+
+def reverse_each_word(sentence)
+  empty_array = []
+  sentence_array = sentence.split(" ")
+  sentence_array.collect { |string|
+  empty_array << string.reverse
+  }
+  empty_array.join(" ")
+end 
