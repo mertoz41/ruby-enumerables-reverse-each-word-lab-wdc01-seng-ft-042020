ccbf576bb4815c74259aa14cb2051953f9652a14
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-04-01 22:37:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence)
   array = string.split(" ")
   new_array = [] 
   array.each { | string |
-  new_array << string.reverse 
-  
+  new_array << string.reverse
   }
+  new_array.join(" ")
 end 
